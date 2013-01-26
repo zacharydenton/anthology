6f67f39cbf443598dee1854ce34d00a8e187af5d
Zach Denton <zacharydenton@gmail.com>
2013-01-26 13:40:22 -0500

    Add Reddit importer and custom m2m relation.

--- app/models.py
+++ app/models.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 class Article(models.Model):
     url = models.URLField(unique=True)
     title = models.CharField(max_length=300)
-    read = models.BooleanField()
     content = models.TextField()
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, through='PersonalArticle')
 
     def __unicode__(self):
         return '%s' % self.url
@@ -19,3 +18,13 @@ class Article(models.Model):
     @property
     def tokens(self):
         return nltk.word_tokenize(self.text)
+
+class PersonalArticle(models.Model):
+    user = models.ForeignKey(User)
+    article = models.ForeignKey(Article)
+    read = models.BooleanField()
+    liked = models.BooleanField()
+
+    def __unicode__(self):
+        return '@%s [%s]' % (self.user, self.article.url)
+
--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import requests
 from readability.readability import Document
-from app.models import Article
+from app.models import Article, PersonalArticle
 
 def import_pinboard(username, password, user):
     api = requests.get('http://api.pinboard.in/v1/posts/all?format=json', auth=requests.auth.HTTPBasicAuth(username, password))
@@ -15,8 +15,47 @@ def import_pinboard(username, password, user):
             article = Article()
             article.url = bookmark['href']
             article.title = bookmark['description']
-            article.read = bookmark['toread'] == 'no'
             article.content = content
             article.save()
-            article.users.add(user)
+
+            personal = PersonalArticle()
+            personal.user = user
+            personal.article = article
+            personal.read = bookmark['toread'] == 'no' 
+            personal.liked = True
+            personal.save()
+
+def import_reddit(username, password, user):
+    def add_link(link, liked):
+        try:
+            r = requests.get(link['url'])
+            if r.status_code != 200: return
+            content = Document(r.text).summary(True)
+        except:
+            return
+        if content:
+            article = Article()
+            article.url = link['url']
+            article.title = link['title']
+            article.content = content
+            article.save()
+
+            personal = PersonalArticle()
+            personal.user = user
+            personal.article = article
+            personal.read = True
+            personal.liked = liked
+            personal.save()
+
+    api = requests.Session()
+    api.headers.update({'User-Agent': 'Anthology 3.14'})
+    api.post('http://www.reddit.com/api/login', {'user': username, 'passwd': password})
+
+    r = api.get('http://www.reddit.com/user/%s/liked.json' % username)
+    for link in r.json()['data']['children']:
+        add_link(link['data'], True)
+
+    r = api.get('http://www.reddit.com/user/%s/disliked.json' % username)
+    for link in r.json()['data']['children']:
+        add_link(link['data'], False)
 
