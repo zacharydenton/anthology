3c89648f0bec1dfb2eccb77fae069f468a1a59cf
Zach Denton <zacharydenton@gmail.com>
2013-01-25 19:51:56 -0500

    Add Pinboard importer.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Article(models.Model):
-    url = models.URLField()
+    url = models.URLField(unique=True)
     title = models.CharField(max_length=300)
     read = models.BooleanField()
     content = models.TextField()
+
+    def __unicode__(self):
+        return '%s' % self.url
 
---
+++ lib.py
@@ -0,0 +1,21 @@
+import requests
+from readability.readability import Document
+from app.models import Article
+
+def import_pinboard(username, password):
+    api = requests.get('http://api.pinboard.in/v1/posts/all?format=json', auth=requests.auth.HTTPBasicAuth(username, password))
+    for bookmark in api.json():
+        try:
+            r = requests.get(bookmark['href'])
+            if r.status_code != 200: continue
+            content = Document(r.text).summary(True)
+        except:
+            continue
+        if content:
+            article = Article()
+            article.url = bookmark['href']
+            article.title = bookmark['description']
+            article.read = bookmark['toread'] == 'no'
+            article.content = content
+            article.save()
+
